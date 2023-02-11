31e2d9b57ca6a217d308c4c07683406762948e40
AllanKirui <nallawilljr@gmail.com>
2023-02-11 13:33:01 +0300

    Add and register TheHero component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <TheHeader />
+
+  <div class="container">
+    <main>
+      <TheHero />
+    </main>
+  </div>
 </template>
 
 <script>
 import TheHeader from "@/components/TheHeader";
+import TheHero from "@/components/TheHero";
 
 export default {
   name: "App",
   components: {
     TheHeader,
+    TheHero,
   },
 };
 </script>
