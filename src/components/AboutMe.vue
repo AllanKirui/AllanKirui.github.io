d0be4130451d9e0fd17e624f0d310c54b3d39925
AllanKirui <nallawilljr@gmail.com>
2023-02-13 14:19:46 +0300

    Add html, styles for the about section

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,123 @@
+<template>
+  <section id="about" class="about">
+    <div class="about__text">
+      <h2 class="about__text-title">about <span class="gray">me</span></h2>
+      <p class="about__text-description text-1">
+        My name is <span class="semi-bold">Allan</span>, and I'm
+        <span class="semi-bold">from Kenya</span>. I've been learning how to
+        code for a while now and have a grown a passion for front-end web
+        development.
+      </p>
+      <p class="about__text-description text-2">
+        These are some of the skills I learned along the way in my journey:
+      </p>
+      <div class="skills-wrapper flex flex-jc-c">
+        <ul class="about__text-skills skills-1">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+        </ul>
+        <ul class="about__text-skills skills-2">
+          <li>SASS</li>
+          <li>Vue</li>
+          <li>SQL Basics</li>
+        </ul>
+        <ul class="about__text-skills skills-3">
+          <li>Git</li>
+          <li>GitHub</li>
+          <li>Responsive Design</li>
+        </ul>
+      </div>
+      <p class="about__text-description text-3">
+        I am currently diving deeper into the VueJS ecosystem whilst learning
+        how to interact with databases using SQL and the importance of data
+        structures and algorithms through JavaScript.
+      </p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.about {
+  margin-bottom: var(--m-step-2);
+  text-align: center;
+}
+
+.about__text {
+  max-width: 700px;
+  margin: 0 auto;
+}
+
+.about__text-title {
+  position: relative;
+  margin-bottom: var(--m-step-0);
+}
+
+.about__text-title::before {
+  content: "";
+  display: block;
+  position: absolute;
+  height: 6px;
+  width: 3.125rem;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: -18px;
+  background: linear-gradient(
+    to left,
+    var(--color-gray),
+    var(--color-pearl-dark-grey)
+  );
+  border-radius: 50px;
+}
+
+.about__text-description {
+  line-height: 1.5;
+}
+
+.about__text-description.text-1 {
+  margin-bottom: var(--m-step--1);
+}
+.about__text-description.text-2 {
+  margin-bottom: 1.5rem;
+}
+
+.skills-wrapper {
+  gap: 3.125rem;
+  margin-bottom: var(--m-step--1);
+  text-align: left;
+}
+
+.about__text-skills li {
+  position: relative;
+  font-weight: 600;
+}
+
+.about__text-skills li::before {
+  content: "▹";
+  position: absolute;
+  top: 50%;
+  left: -1.375rem;
+  transform: translateY(-50%);
+  color: var(--color-pearl-dark-grey);
+  font-size: var(--step-0);
+  line-height: 12px;
+}
+
+@media (max-width: 768px) {
+  .about__text-description {
+    text-align: left;
+  }
+
+  .skills-wrapper {
+    justify-content: left;
+    margin-left: 2rem;
+  }
+}
+
+@media (max-width: 480px) {
+  .skills-wrapper {
+    flex-direction: column;
+    gap: 0;
+  }
+}
+</style>
