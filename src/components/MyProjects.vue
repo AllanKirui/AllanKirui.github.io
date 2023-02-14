c88fcb6cecff288e46899873881a43bc77f1f979
AllanKirui <nallawilljr@gmail.com>
2023-02-14 15:22:22 +0300

    Add html, styles for my projects section

---
+++ src/components/MyProjects.vue
@@ -0,0 +1,17 @@
+<template>
+  <section id="projects" class="projects">
+    <h2 class="projects__title">
+      some things I've built and <span class="gray">helped build </span>
+    </h2>
+  </section>
+</template>
+
+<style scoped>
+.projects {
+  margin-bottom: var(--m-step-2);
+}
+
+.projects__title {
+  text-align: center;
+}
+</style>
