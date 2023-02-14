30df118810378b8f30ef563e31fe073a661c85f2
AllanKirui <nallawilljr@gmail.com>
2023-02-14 17:04:28 +0300

    Add html, styles for the footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer>
+    <small>Designed & Built by Allan Kirui.</small>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  width: 100%;
+  height: 6vh;
+  text-align: center;
+}
+</style>
