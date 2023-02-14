1b44b60997c055bdf167cd046e6a1131a8c829c4
AllanKirui <nallawilljr@gmail.com>
2023-02-14 16:50:47 +0300

    Add html, styles for contact me section

---
+++ src/components/ContactMe.vue
@@ -0,0 +1,84 @@
+<template>
+  <section id="contact" class="contact">
+    <div class="contact__text">
+      <h2 class="contact__text-title">
+        feel free to <span class="gray">contact me</span>
+      </h2>
+      <p class="contact__text-description text-1">
+        Connect with me and let's build a world of websites together. I am
+        seeking new opportunities and exciting challenges.
+      </p>
+      <p class="contact__text-description text-2">
+        Feel free to also just <span class="semi-bold">say hi</span> and I'll
+        try me best to get back to you soon!
+      </p>
+    </div>
+
+    <div class="contact__link">
+      <a href="mailto:allan.kirui@outlook.com" class="cta">Say Hello</a>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.contact {
+  margin-bottom: var(--m-step-2);
+  min-height: 92vh;
+  text-align: center;
+}
+
+.contact__text-title {
+  text-align: center;
+}
+
+.contact__text {
+  margin-bottom: 5rem;
+}
+
+.contact__text-title {
+  text-align: center;
+}
+
+.contact__text-description.text-1 {
+  max-width: 490px;
+  margin: 0 auto;
+  margin-bottom: var(--m-step--1);
+}
+
+.contact__link {
+  height: fit-content;
+}
+
+.contact__link .cta {
+  display: inline-block;
+  padding: 0.75rem 2rem;
+  border-radius: 10px;
+  background-color: var(--color-black-brown);
+  color: var(--color-cultured);
+  font-size: var(--step-1);
+  font-weight: 500;
+  transition: all var(--transition-200);
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+}
+
+.contact__link .cta:hover {
+  background-color: var(--color-white);
+  color: var(--color-black-brown);
+  outline: 1px solid var(--color-black-brown);
+  outline-offset: 5px;
+}
+
+@media (max-width: 768px) {
+  .contact {
+    text-align: left;
+  }
+
+  .contact__text-description.text-1 {
+    max-width: 100%;
+  }
+
+  .contact__link {
+    text-align: center;
+  }
+}
+</style>
