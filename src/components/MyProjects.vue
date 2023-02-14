0bbe0b36b57fe6658f263c2758c55525699f7f02
AllanKirui <nallawilljr@gmail.com>
2023-02-14 15:27:43 +0300

    Add and register the ProjectItem component

--- src/components/MyProjects.vue
+++ src/components/MyProjects.vue
@@ -3,8 +3,40 @@
     <h2 class="projects__title">
       some things I've built and <span class="gray">helped build </span>
     </h2>
+
+    <div class="project__cards">
+      <ProjectItem
+        v-for="(project, index) in projectsData"
+        :key="index"
+        :title="project.title"
+        :tag="project.tag"
+        :description="project.description"
+        :about-tags="project.about"
+        :live-link="project.links.live"
+        :repo-link="project.links.repo"
+        :img="project.img"
+        :is-collab="project.isCollab"
+      />
+    </div>
   </section>
 </template>
+
+<script>
+import data from "@/projects-data";
+
+import ProjectItem from "@/components/ProjectItem";
+
+export default {
+  components: {
+    ProjectItem,
+  },
+  data() {
+    return {
+      projectsData: data,
+    };
+  },
+};
+</script>
 
 <style scoped>
 .projects {
