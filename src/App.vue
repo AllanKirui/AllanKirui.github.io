e2b83f698ffb97d9037e2aeea1d7fa9d56f51b50
AllanKirui <nallawilljr@gmail.com>
2023-02-18 12:50:36 +0300

    Add logic to detect color scheme and switch theme

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <TheHeader />
+  <TheHeader @change-theme="switchTheme" />
 
   <div class="container">
     <main>
-      <TheHero />
+      <TheHero :theme="theme" />
       <AboutMe />
       <MyProjects />
       <ContactMe />
@@ -30,6 +30,56 @@ export default {
     MyProjects,
     ContactMe,
     TheFooter,
+  },
+  data() {
+    return {
+      theme: null,
+    };
+  },
+  methods: {
+    switchTheme(el) {
+      const themeToggler = el;
+      const isChecked = themeToggler.checked;
+
+      if (!isChecked) {
+        localStorage.setItem("theme", "light");
+        document.documentElement.setAttribute("data-theme", "light");
+        themeToggler.checked = false;
+        this.theme = "light";
+      } else {
+        localStorage.setItem("theme", "dark");
+        document.documentElement.setAttribute("data-theme", "dark");
+        themeToggler.checked = true;
+        this.theme = "dark";
+      }
+    },
+    detectColorScheme() {
+      let theme = "light";
+
+      // localStorage can be used to override OS theme settings
+      if (localStorage.getItem("theme")) {
+        if (localStorage.getItem("theme") === "dark") {
+          theme = "dark";
+        }
+      } else if (!window.matchMedia) {
+        // check if the matchMedia() method is supported
+        return false;
+      } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        // if the OS theme setting matches 'dark'
+        theme = "dark";
+      }
+
+      // if dark theme is preferred, update the document with the 'dark-theme' attribute
+      if (theme === "dark") {
+        document.documentElement.setAttribute("data-theme", "dark");
+      } else if (theme === "light") {
+        document.documentElement.setAttribute("data-theme", "light");
+      }
+    },
+  },
+  created() {
+    // check if the user has set a theme preference
+    this.detectColorScheme();
   },
 };
 </script>
